50a016c0e6979c8fd20492a70add3a24d6565fa2
Olhighjay <olhighjayartrage@gmail.com>
2021-08-17 14:23:43 +0100

    updating video upload progress

--- app/Jobs/Videos/ConverForStreaming.php
+++ app/Jobs/Videos/ConverForStreaming.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs\Videos;
 
+use FFMpeg;
+use FFMpeg\Format\Video\X264;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -34,6 +36,29 @@ class ConverForStreaming implements ShouldQueue
      */
     public function handle()
     {
-        echo 'converted';
+        // Quality of video to show based on quality of user's internet connection
+        $low = (new X264('aac'))->setKiloBitrate(100);
+        $mid = (new X264('aac'))->setKiloBitrate(250);
+        $high = (new X264('aac'))->setKiloBitrate(500);
+
+
+        //Specify the disk we are taking the video from
+        FFMpeg::fromDisk('local')
+        // We open the file
+        ->open($this->video->path)
+        //We export for Http Live Streaming
+        ->exportForHLS()
+        // Updating the video uploading progress by percentage
+        ->onProgress(function ($percentage) {
+            $this->video->update([
+                'percentage' => $percentage
+            ]);
+        })
+        // We add the 3 different formats we want the video to be in
+        ->addFormat($low)
+        ->addFormat($mid)
+        ->addFormat($high)
+        ->save("public/videos/{$this->video->id}/{$this->video->id}.m3u8");
+
     }
 }
