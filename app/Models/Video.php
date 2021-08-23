c96e952221b360f71711f150188fcaa85a824532
Olhighjay <olhighjayartrage@gmail.com>
2021-08-23 21:16:02 +0100

    introduction to polymorphism

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,13 +8,20 @@ class Video extends Model
 {
     use HasFactory;
 
+
     public function channel() {
         return $this->belongsTo(Channel::class);
     }
 
+
     public function editable() {
         return auth()->check() && $this->channel->user_id;
 
+    }
+
+
+    public function votes() {
+        return $this->morphMany(Vote::class, 'voteable');
     }
 
 
